81376c0b3965125147ff62769e6f90f71d97b0af
jonibek99 <jonibekannaev5@gmail.com>
2025-05-01 20:43:50 +0500

    math7

--- modul07.py
+++ modul07.py
@@ -11,3 +11,6 @@ def calculate_ceiling_floor(number):
     Returns:
         tuple: A tuple containing the ceiling and floor values of the number.
     """
+    return(math.ceil(number),math.floor(number))
+a=float(input())
+print(calculate_ceiling_floor(a))
